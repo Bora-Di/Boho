7d0210a4ec9740be8f4288a1ab02d0e32faf3624
Bora-Di <mbou7841@gmail.com>
2024-07-15 13:06:25 +0100

    about flex -contact-flex

--- Boofolio/src/components/About/About.jsx
+++ Boofolio/src/components/About/About.jsx
@@ -8,8 +8,13 @@ export const About = () => {
     <section className={styles.container} id="about">
       <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
+      <div className={styles.aboutText}>
+          <p>
+          Hello, I'm Bouchra, a junior software engineer graduated from ENSAH. Passionate about creating intuitive and visually appealing user experiences. Equipped with a foundation in programming languages and frameworks such as HTML, CSS, , PHP, Laravel, JAVA and JavaScript. Dedicated to continuous growth, actively seeking opportunities to expand both technical and design skills. Enthusiastic about leveraging the combination of software engineering and graphic design expertise to craft engaging and impactful digital experiences.
+          </p>
+        </div>
+
         <div className={styles.imageContainer}>
-      
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/frontend.png")} alt="front end icon" />
@@ -29,20 +34,11 @@ export const About = () => {
               <h3>Graphic Designer</h3>
             </div>
           </li>
-            <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/design.png")} alt="desillustartionign icon" />
-            <div className={styles.aboutItemText}>
-              <h3>Illustator Designer</h3>
-            </div>
-          </li>
+           
         </ul>
 
         </div>
-        <div className={styles.aboutText}>
-          <p>
-          Hello, I'm Bouchra, a junior software engineer graduated from ENSAH. Passionate about creating intuitive and visually appealing user experiences. Equipped with a foundation in programming languages and frameworks such as HTML, CSS, , PHP, Laravel, JAVA and JavaScript. Dedicated to continuous growth, actively seeking opportunities to expand both technical and design skills. Enthusiastic about leveraging the combination of software engineering and graphic design expertise to craft engaging and impactful digital experiences.
-          </p>
-        </div>
+       
       </div>
     </section>
   );
